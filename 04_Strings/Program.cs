3e5a835e64373f06d0d2c78171630fe233e940fd
Mercedez Bray <mercedezbray@gmail.com>
2022-02-02 12:26:00 -0500

    adding 04_Strings

---
+++ 04_Strings/Program.cs
@@ -0,0 +1,16 @@
+﻿string first = "The cars we sell are";
+string second = "BMW, Lexus, Mercedes and Tesla";
+
+string concatenatedResult = first + " " + second;
+
+Console.WriteLine(concatenatedResult);
+
+string firstName = "Jenn";
+string lastName = "Williams";
+
+string compositeResult = string.Format("Her name is {0} {1}", firstName, lastName);
+
+Console.WriteLine(compositeResult);
+
+string interpolationResult = $"Her name is still {firstName} {lastName}!";
+Console.WriteLine(interpolationResult);
