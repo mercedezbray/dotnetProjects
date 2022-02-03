dd2c56746c17885245162d95633e88a6981d2264
Mercedez Bray <mercedezbray@gmail.com>
2022-02-03 10:18:37 -0500

    adding 06_Conditionals/IfStatements

---
+++ 06_Conditionals/IfStatements/Program.cs
@@ -0,0 +1,42 @@
+﻿bool isSnowing = true;
+bool isGoingOutside = true;
+//if(true/false value)
+//{
+// body of the code, execute this code if above is true
+//}
+if(isSnowing)
+{
+    Console.WriteLine("It is snowing out!");
+}
+
+if(isSnowing && isGoingOutside)
+{
+    Console.WriteLine("Better wear some snow boots!!!");
+}
+
+if(!isSnowing || !isGoingOutside)
+{
+    Console.WriteLine("I do not need any boots!");
+}
+
+if(isSnowing ^ isGoingOutside)
+{
+    Console.WriteLine("I will only go outside once the snow stops");
+}
+
+if(!isSnowing){
+    Console.WriteLine("There is no need to shovel snow!!");
+}
+else{
+    Console.WriteLine("I guess I need to shovel the snow!");
+}
+
+if(isSnowing){
+    Console.WriteLine("I guess I have to go clear off the car");
+}
+else if(!isSnowing){
+    Console.WriteLine("I don't want to clear off the car!");
+}
+else{
+    Console.WriteLine("I don't know what we are even talking about");
+}
