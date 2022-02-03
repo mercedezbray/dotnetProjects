a1fe3c1a2586b9a1555b362d971776ed3d4dc4fd
Mercedez Bray <mercedezbray@gmail.com>
2022-02-03 10:31:47 -0500

    adding 06_Ternaries

---
+++ 06_Ternaries/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Enter your age: ");
+string response = System.Console.ReadLine();
+int age = int.Parse(response);
+
+string output = age >= 18 ? "You can vote!!" : "You're too young to vote";
+
+Console.WriteLine(output);
